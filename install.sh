9fd32e21667c778c5674fe93d6b5a9874a24c9c6
Tim Monks <t.r.monks@gmail.com>
2018-11-10 15:58:06 -0800

    Add i3 config to dot files

--- install.sh
+++ install.sh
@@ -5,5 +5,9 @@ set -e
 SCRIPT=$(readlink -f $0)
 DOTFILES_DIR=$(dirname $SCRIPT)
 
+mkdir -p $HOME/.config/i3/
+
 ln -s $DOTFILES_DIR/.vimrc $HOME/.vimrc
 ln -s $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
+ln -s $DOTFILES_DIR/i3_config $HOME/.config/i3/config
+
