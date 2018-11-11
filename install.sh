e8e8e968848ae8f9b4dfb2f10d6ce6045c1af404
Tim Monks <t.r.monks@gmail.com>
2018-11-10 16:04:11 -0800

    Add clipit config for syncing buffers.

--- install.sh
+++ install.sh
@@ -6,8 +6,10 @@ SCRIPT=$(readlink -f $0)
 DOTFILES_DIR=$(dirname $SCRIPT)
 
 mkdir -p $HOME/.config/i3/
+mkdir -p $HOME/.config/clipit/
 
 ln -s $DOTFILES_DIR/.vimrc $HOME/.vimrc
 ln -s $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
 ln -s $DOTFILES_DIR/i3_config $HOME/.config/i3/config
+ln -s $DOTFILES_DIR/clipitrc $HOME/.config/clipit/clipitrc
 
