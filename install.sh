52efa796dedf40ce37ce82b9935b6db384223df6
Tim Monks <t.r.monks@gmail.com>
2018-11-10 18:16:02 -0800

    urxvt extension for font resizing.

--- install.sh
+++ install.sh
@@ -7,10 +7,11 @@ DOTFILES_DIR=$(dirname $SCRIPT)
 
 mkdir -p $HOME/.config/i3/
 mkdir -p $HOME/.config/clipit/
+mkdir -p $HOME/.urxvt/ext
 
 ln -is $DOTFILES_DIR/.vimrc $HOME/.vimrc
 ln -is $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
 ln -is $DOTFILES_DIR/i3_config $HOME/.config/i3/config
 ln -is $DOTFILES_DIR/clipitrc $HOME/.config/clipit/clipitrc
 ln -is $DOTFILES_DIR/i3status_conf $HOME/.i3status.conf
-
+ln -is $DOTFILES_DIR/urxvt-extensions/font-size $HOME/.urxvt/ext/font-size
