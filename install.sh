703e3f0d2892df7eb43e597666e02f644be8506c
Tim Monks <t.r.monks@gmail.com>
2018-11-10 16:05:38 -0800

    Prompt for replacing links

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ DOTFILES_DIR=$(dirname $SCRIPT)
 mkdir -p $HOME/.config/i3/
 mkdir -p $HOME/.config/clipit/
 
-ln -s $DOTFILES_DIR/.vimrc $HOME/.vimrc
-ln -s $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
-ln -s $DOTFILES_DIR/i3_config $HOME/.config/i3/config
-ln -s $DOTFILES_DIR/clipitrc $HOME/.config/clipit/clipitrc
+ln -is $DOTFILES_DIR/.vimrc $HOME/.vimrc
+ln -is $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
+ln -is $DOTFILES_DIR/i3_config $HOME/.config/i3/config
+ln -is $DOTFILES_DIR/clipitrc $HOME/.config/clipit/clipitrc
 
