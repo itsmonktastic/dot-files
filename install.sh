bd7ff92b1c0529a6e82fd634c6d40d0cf038e643
Tim Monks <t.r.monks@gmail.com>
2019-06-02 22:04:37 -0700

    Add offlineimap config that can fetch gmail.

--- install.sh
+++ install.sh
@@ -15,3 +15,4 @@ ln -is $DOTFILES_DIR/i3_config $HOME/.config/i3/config
 ln -is $DOTFILES_DIR/clipitrc $HOME/.config/clipit/clipitrc
 ln -is $DOTFILES_DIR/i3status_conf $HOME/.i3status.conf
 ln -is $DOTFILES_DIR/urxvt-extensions/font-size $HOME/.urxvt/ext/font-size
+ln -is $DOTFILES_DIR/offlineimap_config $HOME/.offlineimaprc
--- setup.sh
+++ setup.sh
@@ -1,1 +1,17 @@
-sudo apt-get -y install i3 i3-status dmenu clipit vim-full feh xbacklight
+#sudo apt-get -y install \
+#  i3 \
+#  i3-status \
+#  dmenu \
+#  clipit \
+#  vim-full \
+#  feh \
+#  xbacklight \
+#  python-keyring
+
+sudo apt-get -y install \
+  clipit \
+  vim \
+  feh \
+  mutt \
+  offlineimap \
+  python-keyring
