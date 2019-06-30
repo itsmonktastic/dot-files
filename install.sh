b7f74afbb41e3dc2789595974a3468de00fa017c
Tim Monks <t.r.monks@gmail.com>
2019-06-30 13:14:48 -0700

    Add some comments to explain the scripts.

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ mkdir -p $HOME/.config/clipit/
 mkdir -p $HOME/.urxvt/ext
 mkdir -p $HOME/.mutt
 
-ln -is $DOTFILES_DIR/.vimrc $HOME/.vimrc
-ln -is $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
-ln -is $DOTFILES_DIR/i3_config $HOME/.config/i3/config
-ln -is $DOTFILES_DIR/clipitrc $HOME/.config/clipit/clipitrc
-ln -is $DOTFILES_DIR/i3status_conf $HOME/.i3status.conf
-ln -is $DOTFILES_DIR/urxvt-extensions/font-size $HOME/.urxvt/ext/font-size
-ln -is $DOTFILES_DIR/offlineimap_config $HOME/.offlineimaprc
-ln -is $DOTFILES_DIR/muttrc $HOME/.muttrc
-ln -is $DOTFILES_DIR/mutt_mailcap $HOME/.mutt/mailcap
-ln -is $DOTFILES_DIR/bash_aliases $HOME/.bash_aliases
-ln -is $DOTFILES_DIR/dotbashrc $HOME/.bashrc
+ln -fs $DOTFILES_DIR/.vimrc $HOME/.vimrc
+ln -fs $DOTFILES_DIR/.Xdefaults $HOME/.Xdefaults
+ln -fs $DOTFILES_DIR/i3_config $HOME/.config/i3/config
+ln -fs $DOTFILES_DIR/clipitrc $HOME/.config/clipit/clipitrc
+ln -fs $DOTFILES_DIR/i3status_conf $HOME/.i3status.conf
+ln -fs $DOTFILES_DIR/urxvt-extensions/font-size $HOME/.urxvt/ext/font-size
+ln -fs $DOTFILES_DIR/offlineimap_config $HOME/.offlineimaprc
+ln -fs $DOTFILES_DIR/muttrc $HOME/.muttrc
+ln -fs $DOTFILES_DIR/mutt_mailcap $HOME/.mutt/mailcap
+ln -fs $DOTFILES_DIR/bash_aliases $HOME/.bash_aliases
+ln -fs $DOTFILES_DIR/dotbashrc $HOME/.bashrc
--- setup.sh
+++ setup.sh
@@ -1,18 +1,31 @@
-#sudo apt-get -y install \
-#  i3 \
-#  i3-status \
-#  dmenu \
-#  clipit \
-#  vim-full \
-#  feh \
-#  xbacklight \
-#  python-keyring
+#!/bin/sh
+#
+# First time setup, install programs needed/expected on Ubuntu.
+#
+# These obscure programs are useful because:
+#
+# clipit - used to sync different linux clipboards
+# feh - used by mutt to show image attachments
+# python-keyring - used to allow offlineimap access to a password
+# w3m - used by mutt to display HTML emails
 
 sudo apt-get -y install \
-  clipit \
+  git \
   vim \
   feh \
-  mutt \
   offlineimap \
+  clipit \
+  mutt \
   python-keyring \
   w3m
+
+# Install a plugin manager for vim (my vimrc expects it)
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+$SCRIPTPATH/install.sh
+
+# trigger installation of vim plugins the first time
+vim -c PlugInstall
