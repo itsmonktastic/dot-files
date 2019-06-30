b730aef56ce0bf1d1de179e78f7754af0923d7c3
Tim Monks <t.r.monks@gmail.com>
2019-06-30 15:47:39 -0700

    Add more system dependencies for ruby.

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -5,10 +5,20 @@
 set -ex
 
 # ruby-dev needed to build native extensions
-# libxml2 needed to build nokogiri
-sudo apt-get install -y ruby ruby-dev libxml2-dev 
+# build-essential because you need C++ toolchain to develop ruby
+# libxml2-dev needed to build nokogiri
+# libsqlite3-dev needed for ruby sqlite
+# libv8-dev for mini_racer/therubyracer gems
+sudo apt-get install -y \
+  ruby \
+  ruby-dev \
+  build-essential \
+  libxml2-dev \
+  libsqlite3-dev \
+  libv8-dev
 
 # install some programs packaged in rubygems into home directory
+# assumption is that ~/.gem/ruby/*/bin is in $PATH
 GEM_INSTALL="gem install --user"
 
 # most of my gemfiles are v1, v2 is unhappy with some of my projects right now
