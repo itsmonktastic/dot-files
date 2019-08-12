bbb4aef7b4f6907966223e312ef04184735145ec
Tim Monks <t.r.monks@gmail.com>
2019-08-11 18:31:29 -0700

    Install emacs + leiningen.

--- setup_clojure.sh
+++ setup_clojure.sh
@@ -4,7 +4,17 @@
 
 set -ex
 
-curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > ~/bin/lein
-chmod a+x ~/bin/lein
+# install emacs
+sudo apt-get install -y emacs
+cp -r dotemacs $HOME/.emacs.d
 
-cp dotemacs ~/.emacs.d
+# install leiningen
+mkdir -p $HOME/bin
+if [ ! -f $HOME/bin/lein ]
+then
+  curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > $HOME/bin/lein
+fi
+chmod a+x $HOME/bin/lein
+
+# this script downloads lein.jar
+$HOME/lein 2>/dev/null
